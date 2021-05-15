bb70be634f4c81bd02805a840eb0cd2bd2583ee5
SDesolator <protoss06@outlook.com>
2021-05-15 23:09:36 +0800

    fix register bug

--- UoB/hillClimbing.py
+++ UoB/hillClimbing.py
@@ -1,16 +1,46 @@
 import random
 
-import matplotlib.pyplot as plt
-list = []
+import numpy
+
+list_contain = numpy.array([], dtype=bool)
+
+
+def probability_change(probability, input_list):
+    changes = 100 * probability
+    changes = int(round(changes, 0))
+
+    for index in range(0, changes):
+
+        pick = random.randint(0, numpy.size(input_list))
+
+        if input_list[pick]:
+            input_list[pick] = False
+
+        else:
+            index -= 1
+
+    return input_list
+
+
+# ==============================================================
 
 random.seed(10)
 
-for i in range(50):
-    a = random.randint(0, 5)
-    list.append(a)
+for i in range(10):
+    a = random.randint(0, 3)
+    list_contain = numpy.append(list_contain, a)
 
-plt.figure(figsize=(5, 2))
+list_contain = numpy.append(list_contain, 4)
+list_contain = numpy.append(list_contain, 5)
 
-plt.plot(list)
+for i in range(6, 0, -1):
+    list_contain = numpy.append(list_contain, i)
 
-plt.show()
+# ---------------------------------------------
+
+list_probability = numpy.array([])
+
+for i in range(0, 100):
+    list_probability = numpy.append(list_probability, True)
+
+list_test = probability_change(0.2, list_probability)
