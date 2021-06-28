092cf7d5a7b47397faff52483f920de99fe66044
BIAO Zheng <protoss06@outlook.com>
2021-06-28 16:09:46 +0800

    MSc project begin

---
+++ MSc Project/read_data.py
@@ -0,0 +1,29 @@
+subjects = {'s1': 'data/Subject1.h5',
+            's2': 'data/Subject2.h5',
+            's3': 'data/Subject3.h5',
+            's4': 'data/Subject4.h5',
+            's5': 'data/Subject5.h5'}
+
+regine_of_interest = {'VC': 'ROI_VC = 1',
+                      'LVC': 'ROI_LVC = 1',
+                      'HVC': 'ROI_HVC = 1',
+                      'V1': 'ROI_V1 = 1',
+                      'V2': 'ROI_V2 = 1',
+                      'V3': 'ROI_V3 = 1',
+                      'V4': 'ROI_V4 = 1',
+                      'LOC': 'ROI_LOC = 1',
+                      'FFA': 'ROI_FFA = 1',
+                      'PPA': 'ROI_PPA = 1'}
+
+voxel = {'VC': 1000,
+         'LVC': 1000,
+         'HVC': 1000,
+         'V1': 500,
+         'V2': 500,
+         'V3': 500,
+         'V4': 500,
+         'LOC': 500,
+         'FFA': 500,
+         'PPA': 500}
+
+Image_Feature = 'data/ImageFeatures.h5'
