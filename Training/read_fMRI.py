396975985cef5bc6b9b8985047d0fe20a211712c
BIAO Zheng <protoss06@outlook.com>
2021-06-28 15:04:03 +0800

    MSc start

--- Training/read_fMRI.py
+++ Training/read_fMRI.py
@@ -1,7 +1,12 @@
-import nibabel
-import bdpy
+import h5py
+import numpy
 
 print("Loading data...")
 
-data = {}
+data = numpy.array([])
 
+with h5py.File('G:/Entrance/Coding_Training/Python Program/MSc project/GenericObjectDecoding/data/Subject1.h5',
+               'r') as h5f:
+    data = numpy.array(h5f)
+
+print(data)
