970596eee494a8431596f126e6e97d0f9cb1b986
SDesolator <protoss06@outlook.com>
2023-01-02 18:55:09 +0800

    learn regular expression

--- fNIRs_EEG_MRI_Read_training/EEG_training.py
+++ fNIRs_EEG_MRI_Read_training/EEG_training.py
@@ -3,11 +3,11 @@ import tensorflow as tf
 import pandas as pd
 import os
 
-path = 'E:/Entrance/Coding/datasets/EEG/Kaggle_EEG_Alcohol/SMNI_CMI_TRAIN/Train'
-
+path = 'E:/Coding/datasets/EEG/Kaggle_EEG_Alcohol/SMNI_CMI_TRAIN/Train/'
 file_name = os.listdir(path)
-file_path = path + '/' + file_name[0]
-print(file_path)
 
-data = pd.read_csv(file_path)
-print(data)
+data = {}
+for name in file_name:
+    data[name] = pd.read_csv(path + name)
+
+
