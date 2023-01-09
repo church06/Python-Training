f23d05a7649f083ba57062fdf326d81b0e31eaa4
SDesolator <protoss06@outlook.com>
2023-01-09 17:36:52 +0800

    learn pytorch

--- fNIRs_EEG_MRI_Read_training/EEG_training.py
+++ fNIRs_EEG_MRI_Read_training/EEG_training.py
@@ -1,13 +1,25 @@
-import mne
-import tensorflow as tf
-import pandas as pd
 import os
+import torch
+import pandas as pd
 
 path = 'E:/Coding/datasets/EEG/Kaggle_EEG_Alcohol/SMNI_CMI_TRAIN/Train/'
 file_name = os.listdir(path)
 
 data = {}
 for name in file_name:
-    data[name] = pd.read_csv(path + name)
 
+    name_num = ''
+    for i in name:
+        if i in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+            name_num += i
 
+    data[int(name_num)] = pd.read_csv(path + name)
+
+for i in range(468):
+    num = i + 1
+    sb_id = data[num]['subject identifier'].unique()
+    if 'c' in sb_id:
+        print(num)
+        break
+
+model = ''
