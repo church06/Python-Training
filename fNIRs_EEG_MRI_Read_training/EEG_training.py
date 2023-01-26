88548b5879c9ffbbe50ffa7252816cdb2cc86a18
SDesolator <protoss06@outlook.com>
2023-01-26 21:43:06 +0800

    process Dataframe

--- fNIRs_EEG_MRI_Read_training/EEG_training.py
+++ fNIRs_EEG_MRI_Read_training/EEG_training.py
@@ -4,30 +4,34 @@ from torch import nn
 import pandas as pd
 
 
+def get_data():
+    path = 'E:/Coding/datasets/EEG/Kaggle_EEG_Alcohol/SMNI_CMI_TRAIN/Train/'
+    file_name = os.listdir(path)
+
+    data = {}
+    data_split = {}
+
+    for name in file_name:
+        name_num = ''
+        for i in name:
+            if i in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+                name_num += i
+        data[int(name_num)] = pd.read_csv(path + name)
+
+    for i in range(468):
+        num = i + 1
+        sb_id = data[num]['subject identifier'].unique()
+        if 'c' in sb_id:
+            print(num)
+            break
+
+    return data
+
+
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()
 
 
-path = 'E:/Coding/datasets/EEG/Kaggle_EEG_Alcohol/SMNI_CMI_TRAIN/Train/'
-file_name = os.listdir(path)
-
-data = {}
-for name in file_name:
-
-    name_num = ''
-    for i in name:
-        if i in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-            name_num += i
-
-    data[int(name_num)] = pd.read_csv(path + name)
-
-for i in range(468):
-    num = i + 1
-    sb_id = data[num]['subject identifier'].unique()
-    if 'c' in sb_id:
-        print(num)
-        break
-
-model = ''
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
