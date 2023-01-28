0f2df34929017427f38e9a228116ca700626b200
SDesolator <protoss06@outlook.com>
2023-01-28 20:28:21 +0800

    Formative data, separate EEG channels

--- fNIRs_EEG_MRI_Read_training/EEG_training.py
+++ fNIRs_EEG_MRI_Read_training/EEG_training.py
@@ -2,30 +2,53 @@ import os
 import torch
 from torch import nn
 import pandas as pd
+import itertools
 
 
 def get_data():
-    path = 'E:/Coding/datasets/EEG/Kaggle_EEG_Alcohol/SMNI_CMI_TRAIN/Train/'
-    file_name = os.listdir(path)
+    path_fn = 'E:/Coding/datasets/EEG/Kaggle_EEG_Alcohol/SMNI_CMI_TRAIN/Train/'
+    file_name_fn = os.listdir(path_fn)
 
-    data = {}
-    data_split = {}
+    data_dict_fn = {}
 
-    for name in file_name:
-        name_num = ''
-        for i in name:
-            if i in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-                name_num += i
-        data[int(name_num)] = pd.read_csv(path + name)
+    for name_fn in file_name_fn:
 
-    for i in range(468):
-        num = i + 1
-        sb_id = data[num]['subject identifier'].unique()
-        if 'c' in sb_id:
-            print(num)
-            break
+        name_num_fn = ''
+        for i_fn in name_fn:
+            if i_fn in ['0', '1', '2', '3', '4',
+                        '5', '6', '7', '8', '9']:
+                name_num_fn += i_fn
 
-    return data
+        name_int_fn = int(name_num_fn)
+
+        data_fn = pd.read_csv(path_fn + name_fn)
+        key_list_fn = data_fn['sensor position'].unique().tolist()
+        sbj_split_fn = {}
+
+        for key_fn in key_list_fn:
+            sbj_split_fn[key_fn] = data_fn[data_fn['sensor position'] == key_fn]
+
+        data_dict_fn[name_int_fn] = sbj_split_fn
+
+        print(f"Subject No.{'%03d' % name_int_fn} State: {data_fn['subject identifier'].unique()}")
+
+    return data_dict_fn
+
+
+def split_data(data_input: dict):
+    data_split_fn = {}
+
+    for i_fn in range(1, 469):
+        sbj_fn = data_input[i_fn]
+        key_list_fn = sbj_fn['sensor position'].unique().tolist()
+        sbj_split_fn = {}
+
+        for key_fn in key_list_fn:
+            sbj_split_fn[key_fn] = sbj_fn[sbj_fn['sensor position'] == key_fn]
+
+        data_split_fn[i_fn] = sbj_split_fn
+
+    return data_split_fn
 
 
 class Model(nn.Module):
@@ -35,3 +58,5 @@ class Model(nn.Module):
 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+data = get_data()
